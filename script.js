9b2f5426d97f2d83e6368295b4292da3a89037c0
Cody Switzer <cswitzer85@gmail.com>
2020-01-30 16:17:04 -0600

    6th commit; date & time display now reads month (as the word), day & year only. hour variable is saved but not displayed.

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-var CurrentTimeAndDate = new Date();
-document.getElementById("currentDay").innerHTML = CurrentTimeAndDate;
+var currentTimeAndDate = new Date();
+var year = currentTimeAndDate.getUTCFullYear();
+var month = currentTimeAndDate.getUTCMonth();
+var day = currentTimeAndDate.getUTCDate();
+var hour = currentTimeAndDate.getUTCHours();
+monthArray = ["January", "Februrary", "March", "April", "May", "June", "July", "August", "Sepetember", "October", "November", "December"]
+document.getElementById("currentDay").innerHTML =(monthArray[month]) + " " + (day) + " " + (year);
